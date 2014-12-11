a621c8ba77803c053f21c44dee4823b952b273d7
rahmandawibowo <rahmandawibowo@gmail.com>
2014-12-11 21:33:07 +0700

    add module and name field to state and add unauthorized app access function

--- public/app/scripts/app.js
+++ public/app/scripts/app.js
@@ -4,32 +4,50 @@ angular.module('imail', ['ui.router'])
 
 	.config(function ($stateProvider, $urlRouterProvider) {
 
-		$urlRouterProvider
-		.rule(function ($injector, $location) {
-			// handle redirect to login page if user hasn't logged in
-		})
-		.otherwise('/imail/inbox');
+		$urlRouterProvider.otherwise('/imail/inbox');
 
 		$stateProvider
 		.state('imail', {
-			url: '/imail',
+			abstract: true,
+			name: 'imail',
+			module: 'private',
+			url: '^/imail',
 			templateUrl : 'partials/imail.html',
 			controller : 'ImailController'
 		})
 		.state('imail.inbox', {
+			name: 'inbox',
+			module: 'private',
 			url: '/inbox',
 			templateUrl : 'partials/inbox.html',
 			controller : 'InboxController'
 		})
 		.state('imail.email', {
-			url: '/inbox/{emailId:[0-9]{1,6}}',
+			name: 'email',
+			module: 'private',
+			url: '/inbox/{emailId:int}',
 			templateUrl : 'partials/email.html',
 			controller : 'EmailController'
 		})
 		.state('login', {
-			url : '/login',
+			name: 'login',
+			module: 'public',
+			url : '^/login',
 			templateUrl : 'partials/login.html',
 			controller : 'LoginController'
 		});
 
-	});
+	})
+
+	.run(['$rootScope', '$state', 'Auth', function ($rootScope, $state, Auth) {
+
+		$rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
+ 
+			if(toState.module === 'private' && !Auth.isAuthenticated()) {
+				event.preventDefault();
+				$state.go('login');
+			} 
+
+		});
+
+	}]);
