066b5cf9160ce8c5b19f19267bc256e01f7c9f14
rahmandawibowo <rahmandawibowo@gmail.com>
2014-12-11 11:57:22 +0700

    add auth filter route to api

--- app/routes.php
+++ app/routes.php
@@ -10,17 +10,27 @@
 | and give it the Closure to execute when that URI is requested.
 |
 */
+Route::filter('auth', function() {
+	if(Auth::guest()) {
+		return Response::json('Need authentication first', 401);
+	}
+});
+
 
 Route::get('/', function()
 {
 	return View::make('hello');
 });
 
-Route::get('/api/inbox', array('uses' => 'EmailController@getByUser'));
-Route::post('/api/inbox', array('uses' => 'EmailController@sendMessage'));
-Route::get('/api/inbox/{emailId}', array('uses' => 'EmailController@getById'));
-Route::get('/api/inbox/updates/{lastEmailId}', array('uses' => 'EmailController@getUpdates'));
+Route::group(array('before' => 'auth'), function() {
+
+	Route::get('/api/inbox', array('uses' => 'EmailController@getByUser'));
+	Route::post('/api/inbox', array('uses' => 'EmailController@sendMessage'));
+	Route::get('/api/inbox/{emailId}', array('uses' => 'EmailController@getById'));
+	Route::get('/api/inbox/updates/{lastEmailId}', array('uses' => 'EmailController@getUpdates'));
+
+});
 
 Route::post('/api/login', array('uses' => 'UserController@login'));
-Route::post('/api/logout', array('uses' => 'UserController@logout'));
+Route::get('/api/logout', array('uses' => 'UserController@logout'));
 
