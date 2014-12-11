7c3982092184fd857bd62b7f828d561400c1a648
rahmandawibowo <rahmandawibowo@gmail.com>
2014-12-11 21:36:01 +0700

    fix json return and http status

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -7,18 +7,18 @@ class UserController extends \BaseController {
 	}
 
 	public function login() {
+
 		if(Auth::attempt(array('account' => Input::get('account'), 'password' => Input::get('password')))) {
 			return Response::json(Auth::user(), 200);
 		} else {
 			return Response::json('Invalid credentials', 400);
 		}
+
 	}
 
 	public function logout() {
-		if(Auth::logout()) {
-			return Response::json('Logout success', 200);
-		} else {
-			return Response::json('Logout failed', 400);
-		}
+
+		Auth::logout();
+		return Response::json('Logout success', 200);
 	}
 }
